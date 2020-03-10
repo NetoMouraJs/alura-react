e29c21eb11520a9e136cab064c2765c498c3e887
Neto Moura <netomourajs@gmail.com>
2020-03-10 20:33:47 -0300

    refactory App Component function method for class object

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  name?:string
 }
 
-export default App;
+class App extends React.Component<IProps> {
+	/**
+   *
+   */
+	constructor(props:IProps) {
+		super(props)
+	}
+	render(){
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+            Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+            Learn React
+					</a>
+				</header>
+			</div>
+		)
+	}
+}
+
+export default App
